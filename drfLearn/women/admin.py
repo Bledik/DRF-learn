b1ebc34aeff2b10ca143bbe6c19657d920298d76
Bledik <levaul8@gmail.com>
2024-09-16 18:58:43 +0600

    #3 - 7:46

--- drfLearn/women/admin.py
+++ drfLearn/women/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import Women, Category
+from women.models import Women
 
 admin.site.register(Women)
-admin.site.register(Category)
--- drfLearn/women/views.py
+++ drfLearn/women/views.py
@@ -1,3 +1,16 @@
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from women.serializers import WomenSerializer
+
+from women.models import Women
+
+# class WomenApiView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer 
+
+class  WomenApiView(APIView):
+    def get(self, request):
+        return Response({'title': "Angelina Jolie"})
